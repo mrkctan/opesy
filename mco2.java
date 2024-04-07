a1b7b327df099102163138e7978068e37c7bb5b1
GEbroski <137403026+GEbroski@users.noreply.github.com>
2024-04-07 19:42:14 +0800

    up

--- mco2.java
+++ mco2.java
@@ -19,128 +19,124 @@ public class Helldivers {
     private static int remainingRegular = 0;
     private static int remainingSuper = 0;
 
-    public static void recruitRegularCitizen(int rcId) throws InterruptedException {
-        System.out.println("Regular Citizen " + rcId + " is signing up");
-        regularSemaphore.release();
-        regularCount++;
-        Thread.sleep(500);
-    }
+    // Main method
+    public static void main(String[] args) {
+        int r = 10; // Example number of regular citizens
+        int s = 6; // Example number of super citizens
 
-    public static void recruitSuperCitizen(int scId) throws InterruptedException {
-        System.out.println("Super Citizen " + scId + " is signing up");
-        superSemaphore.release();
-        superCount++;
-        Thread.sleep(500);
-    }
-
-    public static void formTeam(int teamId) throws InterruptedException {
-        regularCount -= 2;
-        superCount -= 2;
-        System.out.println("Team " + teamId + " is ready and now launching to battle (sc: 2 | rc: 2)");
-        teamReadySemaphore.release();
-        Thread.sleep(500);
-    }
-
-    public static void processRegularCitizen() throws InterruptedException {
-        remainingRegular++;
-        regularSemaphore.acquire();
-        if (regularCount >= 2 && superCount >= 2) {
-            formTeam(teamId);
-        } else {
-            remainingRegular--;
-        }
-    }
-
-    public static void processSuperCitizen() throws InterruptedException {
-        remainingSuper++;
-        superSemaphore.acquire();
-        if (regularCount >= 2 && superCount >= 2) {
-            formTeam(teamId);
-        } else {
-            remainingSuper--;
-        }
-    }
-
-    public static void launchTeams() throws InterruptedException {
-        while (true) {
-            teamReadySemaphore.acquire();
-            teamCountSemaphore.acquire();
-            if (remainingRegular < 2 || remainingSuper < 2) {
-                break;
-            }
-            remainingRegular -= 2;
-            remainingSuper -= 2;
-            teamId++;
-            teamCountSemaphore.release();
-        }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        int r = 0, s = 0;
-
-        try (java.util.Scanner scanner = new java.util.Scanner(System.in)) {
-            System.out.print("Enter the number of Regular Citizens: ");
-            r = scanner.nextInt();
-            System.out.print("Enter the number of Super Citizens: ");
-            s = scanner.nextInt();
-        } catch (Exception e) {
-            System.out.println("Invalid input. Please enter integers.");
-            System.exit(1);
-        }
-
+        // Create threads for regular and super citizens
         Thread[] threads = new Thread[r + s];
         for (int i = 0; i < r; i++) {
-            final int rcId = i + 1;
-            threads[i] = new Thread(() -> {
-                try {
-                    recruitRegularCitizen(rcId);
-                    processRegularCitizen();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
+            threads[i] = new Thread(() -> recruitRegularCitizen(i + 1));
+            threads[i].start();
         }
         for (int i = 0; i < s; i++) {
-            final int scId = i + 1;
-            threads[i + r] = new Thread(() -> {
-                try {
-                    recruitSuperCitizen(scId);
-                    processSuperCitizen();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
+            threads[r + i] = new Thread(() -> recruitSuperCitizen(i + 1));
+            threads[r + i].start();
         }
 
-        // Create a thread to launch teams
-        Thread launchThread = new Thread(() -> {
-            try {
-                launchTeams();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        // Start all citizen threads
-        for (Thread t : threads) {
-            t.start();
-        }
-
-        // Start the launch thread
+        // Launch teams
+        Thread launchThread = new Thread(Helldivers::launchTeams);
         launchThread.start();
 
-        // Wait for all citizen threads to complete
-        for (Thread t : threads) {
-            t.join();
+        // Wait for all threads to finish
+        try {
+            for (Thread t : threads) {
+                t.join();
+            }
+            launchThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        // Wait for launch thread to complete
-        launchThread.join();
-
-        // Display remaining citizens
+        // Display final results
         System.out.println("\nFinal Results:");
         System.out.println("Total teams sent: " + (teamId - 1));
         System.out.println("Remaining Regular Citizens: " + remainingRegular);
         System.out.println("Remaining Super Citizens: " + remainingSuper);
+    }
+
+    // Regular citizen signing up
+    private static void recruitRegularCitizen(int rcId) {
+        System.out.println("Regular Citizen " + rcId + " is signing up");
+        regularSemaphore.release();
+        regularCount++;
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Super citizen signing up
+    private static void recruitSuperCitizen(int scId) {
+        System.out.println("Super Citizen " + scId + " is signing up");
+        superSemaphore.release();
+        superCount++;
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Form a team
+    private static void formTeam(int teamId) {
+        regularCount -= 2;
+        superCount -= 2;
+        System.out.println("Team " + teamId + " is ready and now launching to battle (sc: 2 | rc: 2)");
+        teamReadySemaphore.release();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Process regular citizens
+    private static void processRegularCitizen() {
+        try {
+            regularSemaphore.acquire();
+            if (regularCount >= 2 && superCount >= 2) {
+                formTeam(teamId);
+            } else {
+                remainingRegular++;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Process super citizens
+    private static void processSuperCitizen() {
+        try {
+            superSemaphore.acquire();
+            if (regularCount >= 2 && superCount >= 2) {
+                formTeam(teamId);
+            } else {
+                remainingSuper++;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Launch teams
+    private static void launchTeams() {
+        while (true) {
+            try {
+                teamReadySemaphore.acquire();
+                teamCountSemaphore.acquire();
+                if (remainingRegular < 2 || remainingSuper < 2) {
+                    break;
+                }
+                remainingRegular -= 2;
+                remainingSuper -= 2;
+                teamId++;
+                teamCountSemaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
